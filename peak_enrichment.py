b12f41fed4abdb40ec520dc654183f3a4319e9f0
skelviper <skelviper@hotmail.com>
2024-09-03 11:12:39 +0800

    add peak_enrichment calculation

---
+++ peak_enrichment.py
@@ -0,0 +1,69 @@
+import numpy as np
+import pandas as pd
+import tqdm
+
+
+def peak_enrichment(frag_df,peak_df,chrom_length,resolution=100,flank=2000,npeaks=2000):
+    """
+    Calculate the enrichment of fragments around peaks
+    Parameters
+        frag_df: a bed-like dataframe with at least 3 columns representing chrom, start, end
+        peak_df: similar to frag_df
+        chrom_length: a dataframe with 2 columns representing chrom, length
+        resolution: resolution of the data
+        flank: the flanking region around the peak
+        npeaks: number of peaks to sample
+    Returns
+        res: a numpy array with shape (npeaks, (flank*2)//resolution+1)
+    """
+    temp_frags = frag_df.copy().iloc[:,0:3]
+    temp_frags.columns = ["chrom","start","end"]
+    temp_peaks = peak_df.copy().iloc[:,0:3]
+    temp_peaks.columns = ["chrom","start","end"]
+    chrom_length = chrom_length.copy()
+    chrom_length.columns = ["chrom","length"]
+
+    temp_peaks = temp_peaks.sample(npeaks)
+    temp_peaks["start"] = ((temp_peaks["start"] + temp_peaks["end"]) / 2 // resolution * resolution).astype(int)
+    temp_peaks["end"] = (temp_peaks["start"] + resolution).astype(int)
+
+    temp_frags["start"] = (temp_frags["start"]//resolution*resolution).astype(int)
+    temp_frags["end"] = temp_frags["end"]//resolution*resolution
+    temp_frags = temp_frags.groupby(["chrom","start","end"]).size().reset_index().rename(columns={0:"count"})
+
+    # join with chrom_length, dense the fragments
+    result_df = pd.DataFrame(columns=["chrom","start"])
+    for index,row in chrom_length.iterrows():
+        chrom = row["chrom"]
+        length = row["length"]
+        positions=np.arange(0,length,resolution).astype(int)
+        temp_df = pd.DataFrame({"chrom":chrom,"start":positions})
+        result_df = pd.concat([result_df,temp_df],ignore_index=True)
+    result_df["end"] = result_df["start"]+resolution
+
+    temp_frags = result_df.merge(temp_frags,how="left",on=["chrom","start","end"]).fillna(0)
+    temp_frags.set_index(["chrom","start","end"],inplace=True)
+
+    res = []
+    for peak_row in tqdm.tqdm(temp_peaks.iterrows()):
+        peak_row = peak_row[1].values
+        try:
+            # generate list of regions for each peak
+            min_start = peak_row[1] - flank
+            max_start = peak_row[2] + flank
+
+            region_select = [np.array([peak_row[0] for i in range( ((flank // resolution) * 2 + 1))]) ,
+                            np.arange(min_start,max_start,resolution),
+                            np.arange(min_start+resolution,max_start+resolution,resolution)]
+            region_select = np.array(region_select).T
+            region_select = pd.DataFrame(region_select).rename(columns={0:"chrom",1:"start",2:"end"})
+            region_select["start"] = region_select["start"].astype(int)
+            region_select["end"] = region_select["end"].astype(int)
+            index = region_select.set_index(["chrom","start","end"]).index
+
+            res.append(temp_frags.loc[index,:]["count"].values)
+        except:
+            continue
+
+    res = np.array(res)
+    return res
